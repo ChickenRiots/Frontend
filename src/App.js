7f7ac5cd86d92249a553f2fce7680b057f9dd521
aaharbaugh <aaronmharbaugh@gmail.com>
2020-01-02 12:12:54 -0600

    frontpage testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { TimelineMax } from "gsap/all";
+import { Timeline } from "react-gsap";
+import { Tween } from "react-gsap";
+
 
 function App() {
+
+  let tl = new TimelineMax();
+
+  useEffect(() => {
+    //focus on main element for hotkey
+    tl.fromTo()
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 class="header layer_one">Chicken Riot</h1>
+        <h1 class="header layer_two">Chicken Riot</h1>
+        <Timeline target={ <div class="chicken">🐔</div>}>
+    
+          <Tween from={{opacity: 0}} to={{opacity: 1}}>
+           
+          </Tween>
+        </Timeline>
+        <div>
+          <input class="text_input" placeholder="room id" type="text"></input>
+          <input class="room_submit" type="submit" value="join"></input>
+        </div>
     </div>
   );
 }
